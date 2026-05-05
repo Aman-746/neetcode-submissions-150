90419284427777e07141a59ce5fe1a3038994a27
Aman Kumar <83234971+Aman-746@users.noreply.github.com>
2026-05-05 22:11:49 +0530

    Add: car-fleet - submission-0

---
+++ Data Structures & Algorithms/car-fleet/submission-0.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int carFleet(int target, int[] position, int[] speed) {
+        int n=position.length;
+        double[][] arr=new double[n][2];
+        for(int i=0;i<n;i++){
+            arr[i][0]=position[i];
+            arr[i][1]=speed[i];
+        }
+        Arrays.sort(arr,(a,b)->Double.compare(a[0],b[0]));
+        double[] time=new double[n];
+        for(int i=0;i<n;i++){
+            time[i]=(target-arr[i][0])/arr[i][1];
+        }
+        double maxi=Double.MIN_VALUE;
+        int ans=0;
+        for(int i=n-1;i>=0;i--){
+            if(time[i]>maxi){
+                ans++;
+                maxi=time[i];
+            }
+        }
+        return ans;
+    }
+}
+
+// Car which is behind is reaching faster to destination than
+// car ahead means they will meet at some point and make
+// a fleet
