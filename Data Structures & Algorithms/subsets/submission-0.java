13571ce37092043c6f2ed98d2c0c1ac8eab6eabb
Aman Kumar <83234971+Aman-746@users.noreply.github.com>
2026-08-14 23:32:31 +0530

    Add: subsets - submission-0

---
+++ Data Structures & Algorithms/subsets/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        // using bit manipulation
+
+        int n=nums.length;
+        List<List<Integer>>res=new ArrayList<>();
+        for(int i=0;i<(1<<n);i++){
+            List<Integer>li=new ArrayList<>();
+            for(int j=0;j<n;j++){
+                if((i&(1<<j))!=0) li.add(nums[j]);
+            }
+            res.add(li);
+        }
+        return res;
+    }
+}
+
+// i=7 (101)
+// j=0  
+//.     100
