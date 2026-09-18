2ef3ebccaffe5147303991e05d8ef3c823790ce4
Aman Kumar <83234971+Aman-746@users.noreply.github.com>
2026-09-19 00:09:43 +0530

    Add: count-number-of-islands - submission-0

---
+++ Data Structures & Algorithms/count-number-of-islands/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    private void dfs(int i, int j, char[][] grid){
+        if(i<0 || i>=grid.length || j<0 || j>=grid[0].length || grid[i][j]=='0') return;
+        if(grid[i][j]=='1') grid[i][j]='0';
+        dfs(i,j-1,grid);
+        dfs(i,j+1,grid);
+        dfs(i-1,j,grid);
+        dfs(i+1,j,grid);
+    }
+
+    public int numIslands(char[][] grid) {
+        int m=grid.length;
+        int n=grid[0].length;
+        int res=0;
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(grid[i][j]=='1'){
+                    // mark all adjacent as visited
+                    res++;
+                    dfs(i,j,grid);
+                }
+            }
+        }
+        return res;
+    }
+}
